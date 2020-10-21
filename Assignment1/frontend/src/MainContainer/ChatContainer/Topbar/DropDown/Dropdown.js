694da15412fd0c20708b369281edba5be93007ed
il20afar <antoinefarley@gmail.com>
2020-10-21 19:24:10 -0400

    (a1_frontend_darkmode): Added support for dark mode and integrated download menu as part of topbar from the two existing download and clear message function

--- Assignment1/frontend/src/MainContainer/ChatContainer/Topbar/DropDown/Dropdown.js
+++ Assignment1/frontend/src/MainContainer/ChatContainer/Topbar/DropDown/Dropdown.js
@@ -1,111 +1,133 @@
-import React, {useEffect, useState} from 'react';
-import {faDownload, faCaretDown, faCaretUp} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import "./Dropdown.scss"
+import React, { useEffect, useState } from "react";
+import {
+  faDownload,
+  faCaretDown,
+  faCaretUp,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./Dropdown.scss";
 import Modal from "./Modal/Modal";
-import {getData, sendData, getMessage} from "../../../../Utils/Utils";
+import { getData, sendData, getMessage } from "../../../../Utils/Utils";
 import Toggle from "./Toggle/Toggle";
 
 const Dropdown = (props) => {
-    const [dropdown, setDropdown] = useState(false)
-    const [custom, setCustom] = useState(false)
-    const [format, setFormat] = useState("text")
-    const [to, setTo] = useState()
-    const [from, setFrom] = useState()
-    const handleClick = () => {
-        setDropdown(!dropdown)
-    }
+  const [dropdown, setDropdown] = useState(props?.active || false);
+  const [custom, setCustom] = useState(false);
+  const [format, setFormat] = useState("text");
+  const [to, setTo] = useState();
+  const [from, setFrom] = useState();
+  const handleClick = () => {
+    setDropdown(!dropdown);
+  };
 
-    const handleClose = () => {
-        setCustom(false);
-    }
+  const handleClose = () => {
+    setCustom(false);
+  };
 
-    const handleOpen = () => {
-        setCustom(true);
-        handleClick();
-    }
+  const handleOpen = () => {
+    setCustom(true);
+    handleClick();
+  };
 
-    const customYesterday = async () => {
-        var yesterdayTo = new Date() - 1;
-        var yesterdayFrom = new Date() - 1;
-        yesterdayFrom.setHours(0, 0, 0, 0);
-        yesterdayTo.setHours(23, 59, 59, 99);
-        yesterdayFrom= yesterdayFrom.toLocaleString();
-        yesterdayTo = yesterdayTo.toLocaleString();
-        if (props.type === "Download")
-            getMessage(yesterdayFrom, yesterdayTo);
-        else
-            deleteMessage(yesterdayFrom,yesterdayTo);
-    };
+  const customYesterday = async () => {
+    var yesterdayTo = new Date() - 1;
+    var yesterdayFrom = new Date() - 1;
+    yesterdayFrom.setHours(0, 0, 0, 0);
+    yesterdayTo.setHours(23, 59, 59, 99);
+    yesterdayFrom = yesterdayFrom.toLocaleString();
+    yesterdayTo = yesterdayTo.toLocaleString();
+    if (props.type === "Download") getMessage(yesterdayFrom, yesterdayTo);
+    else deleteMessage(yesterdayFrom, yesterdayTo);
+  };
 
-    const customToday = async () => {
-        var todayTo = new Date();
-        var todayFrom = new Date();
-        todayFrom.setHours(0, 0, 0, 0);
-        todayTo.setHours(23, 59, 59, 99);
-        todayFrom = todayFrom.toLocaleString();
-        todayTo = todayTo.toLocaleString();
-        if (props.type === "Download")
-            getMessage(todayFrom, todayTo);
-        else
-            deleteMessage(todayFrom, todayTo);
-    }
+  const customToday = async () => {
+    var todayTo = new Date();
+    var todayFrom = new Date();
+    todayFrom.setHours(0, 0, 0, 0);
+    todayTo.setHours(23, 59, 59, 99);
+    todayFrom = todayFrom.toLocaleString();
+    todayTo = todayTo.toLocaleString();
+    if (props.type === "Download") getMessage(todayFrom, todayTo);
+    else deleteMessage(todayFrom, todayTo);
+  };
 
+  const deleteMessage = async () => {
+    const response = await getData({
+      format: format,
+      from: from,
+      to: to,
+      delete: "delete",
+    });
+  };
 
-    const deleteMessage = async () => {
-        const response = await getData({format: format, from: from, to: to, delete: "delete"});
-    }
+  const getMessage = async (from, to) => {
+    const response = await getData({
+      format: format,
+      from: from,
+      to: to,
+      download: "download",
+    });
+    // console.log(from.toLocaleDateString());
+    // console.log(to);
+    // console.log(response);
+    const filename = response.headers
+      .get("Content-Disposition")
+      .split("filename=")[1];
+    response.blob().then((blob) => {
+      let url = window.URL.createObjectURL(blob);
+      let a = document.createElement("a");
+      a.href = url;
+      a.download = filename;
+      a.click();
+    });
+  };
 
-    const getMessage = async (from, to) => {
-        const response = await getData({format: format, from: from, to: to, download: "download"});
-        // console.log(from.toLocaleDateString());
-        // console.log(to);
-        // console.log(response);
-        const filename = response.headers.get('Content-Disposition').split('filename=')[1];
-        response.blob().then(blob => {
-            let url = window.URL.createObjectURL(blob);
-            let a = document.createElement('a');
-            a.href = url;
-            a.download = filename;
-            a.click();
-        });
-    };
+  const toggleFormat = () => {
+    if (format === "text") setFormat("xml");
+    else setFormat("text");
+    console.log(format);
+  };
 
-    const toggleFormat = () => {
-        if (format === "text")
-            setFormat("xml");
-        else
-            setFormat("text");
-        console.log(format);
-    }
-
-
-    return (
-        <div className={`dropdown-container ${props.darkMode}`}>
-            <div className={dropdown ? "dropdown-icon dropdown-enabled" : "dropdown-icon"} onClick={handleClick}>
-                <FontAwesomeIcon icon={props.type === "Download" ? faDownload : ['fas', 'trash']}/> <span
-                className={"md"}>{props.type} Messages</span>
-                <FontAwesomeIcon icon={dropdown ? faCaretUp : faCaretDown} className={"md"}/>
-                <FontAwesomeIcon className={"xs-right-caret-icon"} icon={['fas', 'caret-right']}/>
-            </div>
-            {dropdown ?
-                <div className={`dropdown-menu ${props.drop}`}>
-                    <ul className="dropdown-menu-list">
-                        <li className={"xs bars"} onClick={handleClick}><FontAwesomeIcon icon={['fas', 'bars']}/></li>
-                        {props.type === "Download" ?
-                            <li><Toggle toggle={toggleFormat} format={format}/></li> :
-                            ""
-                        }
-                        <li className="dropdown-menu-item" onClick={customToday}>Today</li>
-                        <li className="dropdown-menu-item" onClick={customYesterday}>Yesterday</li>
-                        <li className="dropdown-menu-item" onClick={handleOpen}>Custom...</li>
-                    </ul>
-                </div> : ""}
-            {custom ? <Modal format={format} onClose={handleClose} darkMode={props.darkMode}/> : ""}
-
+  return (
+    <div className={`dropdown ${props.darkMode}`}>
+      {dropdown ? (
+        <div className={`dropdown-menu ${props.drop}`}>
+          <ul className="dropdown-menu-list">
+            <li className={"xs bars"} onClick={handleClick}>
+              <FontAwesomeIcon icon={["fas", "bars"]} />
+            </li>
+            {props.type === "Download" ? (
+              <li>
+                <Toggle toggle={toggleFormat} format={format} />
+              </li>
+            ) : (
+              ""
+            )}
+            <li className="dropdown-menu-item" onClick={customToday}>
+              Today
+            </li>
+            <li className="dropdown-menu-item" onClick={customYesterday}>
+              Yesterday
+            </li>
+            <li className="dropdown-menu-item" onClick={handleOpen}>
+              Custom...
+            </li>
+          </ul>
         </div>
-    );
-}
+      ) : (
+        ""
+      )}
+      {custom ? (
+        <Modal
+          format={format}
+          onClose={handleClose}
+          darkMode={props.darkMode}
+        />
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 
 export default Dropdown;
-
--- Assignment1/frontend/src/MainContainer/ChatContainer/Topbar/Topbar.js
+++ Assignment1/frontend/src/MainContainer/ChatContainer/Topbar/Topbar.js
@@ -1,37 +1,201 @@
-import React, {useEffect, useState} from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons'
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled, { useTheme } from "styled-components";
+import {
+  faSun,
+  faMoon,
+  faDownload,
+  faTrash,
+  faChevronCircleDown,
+} from "@fortawesome/free-solid-svg-icons";
 import Dropdown from "./DropDown/Dropdown";
-import "./TopBar.scss"
+import "./TopBar.scss";
 
+const StyledCaretDropdown = styled.div`
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+  height: 100px;
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  outline: none;
+  .icon-container {
+    transition: 0.25s ease-in-out;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .chevron-down {
+      width: 70px;
+      height: 70px;
+    }
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+  
+  .dropdown-container {
+    transition: 0.4 ease;
+    position: absolute;
+    top: 90px;
+    left: 20px;
+    width: 600px;
+    height: fit-content;
+    border-radius: 8px;
+    background-color: white;
+    border: white 2px solid;
+    padding: 4px;
+    display: flex;
+    flex-direction: column;
 
+    .actions-container{
+        width 100%;
+        height: fit-content;
+        display: flex;
+        .action-toggle{
+            height: 40px;
+            width: 50%;
+            margin: 5px;
+            border-radius: 5px;
+            text-align: center;
+            font-size: 24px;
+            line-height: 40px;
+        }
+
+    }
+    .function-container{
+        width: 100%;
+        height: fit-content;
+        padding: 8px;
+        .dropdown{
+            width: calc(100% - 16px);
+            height: fit-content;
+            position: relative;
+            top: 0;
+            .dropdown-menu-list{
+                margin-top:0;
+                margin-bottom:0;
+            }
+        }
+
+    }
+  }
+  &.show {
+    .icon-container {
+      transform: rotate(-180deg);
+      transition: 0.15s ease-in-out;
+    }
+  }
+`;
+
+const CaretDropdown = (props) => {
+  const { darkMode } = props;
+  const [show, setShow] = useState(false);
+  const [functionMenu, setFunctionMenu] = useState("download");
+
+  const handleClick = (type) => {
+    setFunctionMenu(type);
+  };
+
+  React.useEffect(() => {}, []);
+
+  const makeActionToggle = (type, icon) => {
+    return (
+      <div
+        className={`action-toggle ${functionMenu === type ? "active" : ""}`}
+        onClick={() => handleClick(type)}
+      >
+        <FontAwesomeIcon icon={icon} />
+        <span className={"md"}>
+          {` ${type[0].toUpperCase()}${type.slice(1)} `}
+          Messages
+        </span>
+      </div>
+    );
+  };
+
+  return (
+    <StyledCaretDropdown
+      className={`${darkMode ? "dark" : "light"} ${show ? "show" : "hide"}`}
+      tabIndex={1}
+      onBlur={() => setShow(false)}
+    >
+      <div className={`icon-container`} onClick={() => setShow(!show)}>
+        <FontAwesomeIcon icon={faChevronCircleDown} size="2x" color={"white"} />
+      </div>
+      {show && (
+        <div className="dropdown-container">
+          <div className={`actions-container`}>
+            {makeActionToggle("download", faDownload)}
+            {makeActionToggle("clear", faTrash)}
+          </div>
+          <div className="function-container">
+            {functionMenu === "download" ? (
+              <Dropdown
+                darkMode={darkMode}
+                active={true}
+                type={"Download"}
+                drop={"dropdown"}
+              />
+            ) : (
+              <Dropdown
+                active={true}
+                darkMode={darkMode}
+                type={"Delete"}
+                drop={"dropdown"}
+              />
+            )}
+          </div>
+        </div>
+      )}
+    </StyledCaretDropdown>
+  );
+};
 
 const Topbar = (props) => {
-    const [darkMode, setDarkMode] = useState(false);
-
-   const handleClick = () => {
-       props.setHideTimes()
-       setDarkMode(!darkMode);
-   }
-   const handleLogout = () => {
-        props.setLoginPage(true);
-   }
-    return (
-        <div className={`topbar${darkMode ? ' dark': ''}`}>
-            <nav className="navbar-nav">
-                <ul className="nav-item">
-                    <li className="nav-list left laptop">
-                        <Dropdown darkMode={darkMode} type={"Download"} drop={"dropdown"}/>
-                    </li>
-                    <li className="nav-list center username">Welcome {props.username}</li>
-                    <ul className="nav-list right">
-                        <li><Dropdown darkMode={darkMode} type={"Clear"} drop={"dropdown"}/></li>
-                        <li onClick={handleClick} className="toggle-darkmode">{darkMode ? <FontAwesomeIcon icon={['fas', 'sun']} title={"Light Mode"} className="light-icon" color="white"/>: <FontAwesomeIcon icon={['fas', 'moon']} title={"Dark Mode"} className="dark-icon" color="white"/> }</li>
-                        <li onClick={handleLogout}><button className="logout">Logout</button></li>
-                    </ul>
-                </ul>
-            </nav>
-        </div>);
+  const { darkMode, setDarkMode } = props;
+  const handleClick = () => {
+    setDarkMode(!darkMode);
+  };
+  const handleLogout = () => {
+    props.setLoginPage(true);
+  };
+  return (
+    <div className={`topbar${darkMode ? " dark" : ""}`}>
+      <div className="navbar-nav">
+        <CaretDropdown darkMode={darkMode} />
+        <div className="username">Hello, ${props.username}</div>
+        <div style={{ width: "fit-content" }}>
+          <ul className="nav-list right">
+            <li>
+              <Dropdown darkMode={darkMode} type={"Clear"} drop={"dropdown"} />
+            </li>
+            <li onClick={handleClick} className="toggle-darkmode">
+              {darkMode ? (
+                <FontAwesomeIcon
+                  icon={["fas", "sun"]}
+                  title={"Light Mode"}
+                  className="light-icon"
+                  color="white"
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={["fas", "moon"]}
+                  title={"Dark Mode"}
+                  className="dark-icon"
+                  color="white"
+                />
+              )}
+            </li>
+            <li onClick={handleLogout}>
+              <button className="logout">Logout</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Topbar;
